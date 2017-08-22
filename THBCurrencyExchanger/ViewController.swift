51213693e83d6b02c41ad422810829566124a54f
kuanhuachen <kuanhuachen@doits-MacBook-Pro.local>
2017-08-22 13:25:08 +0800

    Initial Commit

---
+++ THBCurrencyExchanger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  THBCurrencyExchanger
+//
+//  Created by kuanhuachen on 2017/8/22.
+//  Copyright © 2017年 kuanhuachen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
